c61c8f8bb279df71fce00b2457900c69c4031796
Ergun Oz <ergun.oz82@gmail.com>
2022-02-16 13:56:46 -0500

    Created new directory named config created new file name db.js under config created a base name for the mongdb created the mongodb uri for development and test Identified development environment for test and development defined port for API to run on
    server.js
    set CORS headers on response from this API using the `cors` NPM package
    `CLIENT_ORIGIN` is an environment variable that will be set on Heroku

--- server.js
+++ server.js
@@ -2,7 +2,15 @@
 const express = require('express')
 //require mongoose
 const mongoose = require('mongoose')
+//require cors
 const cors = require('cors')
+//require database configuaration logic
+//db will be the actual Mongo URI as a string
+const db = require('./config/db')
+// define server and client ports
+// used for cors and local port declaration
+const serverDevPort = 4741
+const clientDevPort = 7165
 
 //requiring userRoutes
 const userRoutes = require('./routes/userRoutes')
@@ -10,13 +18,20 @@ const doctorRoutes = require('./routes/doctorRoutes')
 const reviewRoutes = require('./routes/reviewRoutes')
 
 //connect to mongodb 
-mongoose.connect('mongodb://127.0.0.1/restaurant_api', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+mongoose.connect(db, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 })
 
 //crate new express application
 const app = express()
+
+// set CORS headers on response from this API using the `cors` NPM package
+// `CLIENT_ORIGIN` is an environment variable that will be set on Heroku
+app.use(cors({ origin: process.env.CLIENT_ORIGIN || `http://127.0.0.1:${clientDevPort}` }))
+
+// define port for API to run on
+const port = process.env.PORT || serverDevPort
 
 // middleware requests
 app.use(express.json())
