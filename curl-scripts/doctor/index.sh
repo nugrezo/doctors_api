fda94ff9a3dbfb381e39175d278cdad3c8ce37b8
Ergun Oz <ergun.oz82@gmail.com>
2022-02-16 09:19:26 -0500

    doctor index.sh created under curl-scripts/doctors directory I tested the api for index - to show all doctors created.
    I created api call to show all doctors inside doctorRoutes.

---
+++ curl-scripts/doctor/index.sh
@@ -0,0 +1,3 @@
+curl "http://127.0.0.1:4741/doctors" \
+  --include \
+  --request GET \
--- routes/doctorRoutes.js
+++ routes/doctorRoutes.js
@@ -14,4 +14,37 @@ router.post('/doctors', (req, res, next) => {
         .catch(next)
 })
 
+//INDEX
+//GET /doctors/ 
+//We will get all doctors created by user
+router.get('/doctors', (req, res, next) => {
+    Doctor.find()
+        // .populate('owner')
+        // .populate('review.owner')
+        .then(doctors => res.json({ doctors: doctors }))
+        .catch(next)
+})
+
+
+
+
+
+
+//SHOW
+// GET/ users/:id
+//We will get a single user with this api request.
+
+
+//UPDATE 
+// PATCH /users/:id
+//We will update user information with this api call.
+
+
+//DELETE
+//DELETE / users/:id
+//We will delete a user information with it`s id with below api call
+
+
+
+
 module.exports = router
